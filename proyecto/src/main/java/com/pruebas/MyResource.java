ab1f5e500df545c4e1302709a190408d0f7ab359
carlosmot <carlos.motag@gmail.com>
2021-06-15 12:49:50 +0200

    cvvccvcv

--- proyecto/src/main/java/com/pruebas/MyResource.java
+++ proyecto/src/main/java/com/pruebas/MyResource.java
@@ -4,8 +4,8 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-
-/**
+    
+/**    
  * Root resource (exposed at "myresource" path)
  */
 @Path("/saludo")
